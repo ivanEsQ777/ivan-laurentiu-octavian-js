2255d2c378d2f96a3c4d2143220a00190b72bedf
ivanEsQ777 <60344569+ivanEsQ777@users.noreply.github.com>
2020-06-09 21:59:54 +0300

    09.06.2020 object.keys

--- M01S04/ex10/app.js
+++ M01S04/ex10/app.js
@@ -79,3 +79,75 @@ person.friends.forEach(function(friend, i, friends) {
 });
 
 console.log(message);
+
+console.warn(' Folosind forEach, afiseaza numarul total de ani pe care il au persoanele din arrayul friends, doar daca au varsta mai mare decat 30 inclusiv.');
+
+var totalAge = 0;
+
+person.friends.forEach(function (friend) {
+
+  if (friend.age >= 30) {
+    totalAge += friend.age;
+  }
+});
+
+console.log(totalAge);
+
+console.warn(' Folosind forEach, afiseaza suma anilor de nastere a persoanelor care au varsta impara. ');
+
+var totalYears = 0;
+
+person.friends.forEach(function (friend) {
+  var friendAge = friend.age;
+
+  var birthYear = (new Date(Date.now()).getFullYear()) - friend.age;
+
+  if (birthYear % 2 !== 0) {
+    totalYears += birthYear;
+  }
+});
+
+console.log(totalYears);
+
+console.warn(' Afiseaza diferenta de varsta dintre persoana si prietenii din arrayul friends daca aceasta este mai mare sau egala cu 2 ani.');
+
+person.friends.forEach(function (friend){
+  var ageDiff = person.age - friend.age;
+
+  if (ageDiff >= 2) {
+    console.log(ageDiff);
+  }
+});
+
+console.warn(' Afiseaza fraza: "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven..');
+
+message = '';
+
+person.friends.forEach(function (friend){
+  var ageDiff = Math.abs(person.age - friend.age);
+
+  message += `Intre ${person.name} si ${friend.name} este o diferenta de ${ageDiff} ani. `;
+});
+
+console.log(message.trim());
+
+console.warn('Folosind metoda reverse si apoi forEach, afiseaza in ordine inversa elementele arrayului skills.');
+
+message = '';
+person.skills.reverse();
+
+person.skills.forEach(function (skill, i, skills) {
+  var punctuation = ', ';
+
+  message += skill;
+
+  if (i === skills.length - 1) {
+    punctuation = '.';
+  }
+
+  message += punctuation;
+});
+
+console.log(message);
+
+person.skills.reverse();
