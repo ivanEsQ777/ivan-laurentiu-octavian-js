6754d22b8de91247888aaa443f51984ad171efe8
ivanEsQ777 <60344569+ivanEsQ777@users.noreply.github.com>
2020-06-23 20:48:58 +0300

    23.06

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -1,5 +1,28 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(() => {
+  let showMessage = (message) => {
+    let messageParagraph = document.createElement('p');
+
+    messageParagraph.innerText = message;
+    messageParagraph.classList.add('message');
+
+    document.body.appendChild(messageParagraph);
+
+    setTimeout(() => {
+      messageParagraph.remove();
+    }, 1000);
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    let stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', () => {
+      console.log('mousul este pe scena');
+      showMessage('Mousul este pe scena');
+    });
+
+    stage.addEventListener('mouseout', () => {
+      console.log('mousul nu este pe scena');
+      showMessage('Mousul nu este pe scena');
+    });
   });
 })();
--- M01S06/ex05/app.js
+++ M01S06/ex05/app.js
@@ -1,5 +1,41 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    let showButton = document.querySelector('#showButton');
+    let toggleEventButton = document.querySelector('#toggleEventButton');
+    let removeButton = document.querySelector('#removeButtons');
+    let eventBound = true;
+
+    let showAlert = () => {
+      alert('Butonul a fost apasat');
+    };
+
+    showButton.addEventListener('click', showAlert);
+
+    toggleEventButton.addEventListener('click', (event) => {
+      let toggleEventButton = event.target;
+      let paragraphElement = document.createElement('p');
+
+      if (eventBound === true) {
+        showButton.removeEventListener('click', showAlert);
+        eventBound = false;
+        toggleEventButton.innerText = 'Porneste afisarea.';
+        paragraphElement.innerText = 'Alerta nu va fi afisata!';
+      } else {
+        showButton.addEventListener('click', showAlert);
+        eventBound = true;
+        toggleEventButton.innerText = 'Opreste afisarea.';
+        paragraphElement.innerText = 'Alerta va fi afisata!';
+      }
+
+      document.body.appendChild(paragraphElement);
+    });
+
+    removeButton.addEventListener('click', () => {
+      let confirmation = confirm('Esti sigur ca vrei sa elimini butoanele?');
+      if (confirmation === true) {
+        showButton.remove();
+        toggleEventButton.remove();
+      }
+    });
   });
 })();
