42e1bc03de77b9c081255bb6cd1ab8cbcf20f0e5
ivanEsQ777 <60344569+ivanEsQ777@users.noreply.github.com>
2020-07-02 21:42:14 +0300

    car js workshop

--- M02S01/ex03/app.js
+++ M02S01/ex03/app.js
@@ -3,16 +3,30 @@ let Car = {
   color: '',
   wheels: '',
   speed: 0,
+  topSpeed: 140,
+  topReverseSpeed: -70,
   displaySpeed: function () {
     console.log(`Viteza curenta este: ${this.speed}`);
   },
   accelerate: function () {
-    this.speed++;
-    this.displaySpeed();
+    this.setSpeed(++this.speed);
   },
   decelerate() {
-    this.speed--;
-    this.displaySpeed;
+    this.setSpeed(--this.speed);
+  },
+  setSpeed: function (speed) {
+    if (speed > this.topSpeed) {
+      this.speed = this.topSpeed;
+      this.displaySpeed();
+      return;
+    }
+    if (speed < this.topReverseSpeed) {
+      this.speed = this.topReverseSpeed;
+      this.displaySpeed();
+      return;
+    }
+    this.speed = speed;
+    this.displaySpeed();
   }
 };
 
@@ -21,5 +35,10 @@ audi.make = 'Audi';
 audi.color = 'black';
 audi.wheels = 4;
 audi.speed = 0;
+audi.topSpeed = 220;
 
 console.log(audi);
+
+// Adauga metoda setSpeed(), proprietatile topSpeed si topReverseSpeed si implementeaza protectiile deja cunoscute.
+// Seteaza topSpeed la 140 apoi ruleaza metoda setSpeed() pentru a face viteza curenta 140. 
+// Ruleaza metoda accelerate().
