a6a65b7cb2dc44bd5be0051fbd8c80f91dd61433
ivanEsQ777 <60344569+ivanEsQ777@users.noreply.github.com>
2020-07-16 22:00:32 +0300

    m02s04 - animations in JS

---
+++ M02S04/ex01/app.js
@@ -0,0 +1,39 @@
+let box = document.querySelector('.box');
+let animationClass = 'animate-class';
+let toggleButton = document.getElementById('apply-style');
+let darkenButton = document.getElementById('darken');
+let isClassApplyed = false;
+
+
+setTimeout(() => {
+  box.classList.add(animationClass);
+  isClassApplyed = true;
+
+  setTimeout(() => {
+    box.classList.remove(animationClass);
+    isClassApplyed = false;
+  }, 2000)
+}, 2000);
+
+toggleButton.addEventListener('click', (event) => {
+  let btn = event.currentTarget;
+  box.classList.toggle(animationClass);
+
+  isClassApplyed = !isClassApplyed;
+
+  btn.innerText = isClassApplyed === true ? 'Elimina' : 'Aplica';
+
+  // if (box.classList.contains(animationClass)) {
+  //   btn.innerText = 'Aplica';
+  // } else {
+  //   btn.innerText = 'Elimina';
+  //   }
+});
+
+darkenButton.addEventListener('click', () => {
+  // box.style.backgroundColor = '#000';
+  box.setAttribute('style', 'background-color: black');
+});
+
+
+
